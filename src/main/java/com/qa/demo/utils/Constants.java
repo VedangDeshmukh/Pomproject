a418ae69ca69f5e0704d078d2c2cbca933352a74
VedangDeshmukh <deshmukhvedang7@gmail.com>
2022-07-21 22:41:10 +0530

    updated the Constant class

--- src/main/java/com/qa/demo/utils/Constants.java
+++ src/main/java/com/qa/demo/utils/Constants.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Constants {
 	
 	
-	public static final String LOGINPAGE_URL = "https://opensource-demo.orangehrmlive.com/index.php/auth/loginn";
+	public static final String LOGINPAGE_URL = "https://opensource-demo.orangehrmlive.com/index.php/auth/login";
 	public static final int DEFAULTIMEOUT = 5;
 	public static final String LOGINPAGE_TITLE = "OrangeHRM";
-	public static final String DASHBOARDPAGE_URL = "https://opensource-demo.orangehrmlive.com/index.php/dashboards";
-	public static final String DASHBOARDPAGE_HEARDER = "Dashboarda";
+	public static final String DASHBOARDPAGE_URL = "https://opensource-demo.orangehrmlive.com/index.php/dashboard";
+	public static final String DASHBOARDPAGE_HEARDER = "Dashboard";
 	
 	
 	public static final List<String> MAINMENULIST = Arrays.asList("Admin","PIM","Leave","Time","Recruitment","My Info","Performance",
@@ -18,7 +18,7 @@ public class Constants {
 	
 	public static final List<String> QUCIKLAUNCHLIST = Arrays.asList("Assign Leave","Leave List","Timesheets","Apply Leave","My Leave","My Timesheet");
 			
-	public static final String CANDIDATEINFOPAGE_HEARDER = "Candidatess";
+	public static final String CANDIDATEINFOPAGE_HEARDER = "Candidates";
 	
 	public static final String CREATECANDIDATE_SUCCESS_MESSAGE = "Successfully Saved";
 
